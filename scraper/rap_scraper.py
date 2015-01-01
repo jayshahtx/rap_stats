57950484d5d94dd164d942437dee676bc4648fa8
jay shah <jay@bayesconsulting.io>
2015-01-01 00:16:17 -0600

    changed to scrape wikipedia

--- scraper/rap_scraper.py
+++ scraper/rap_scraper.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 """Scrapes top rap artists and their top 100 tracks
 """
-#url navigation/scraping
-import urllib2
-import urllib
+#url scraping
 from bs4 import BeautifulSoup
 from selenium import webdriver
+
+#url processing
+import urllib2
+import urllib
 from time import sleep
 from random import random
 import re
-#run selenium headless
-from pyvirtualdisplay import Display
 
 #debugging
 import pdb
@@ -39,7 +39,7 @@ def raw_text(text):
 """
 	Function which scrapes the top 388 rap albums' artist form Rate Your Music
 """
-def get_rap_artists():
+def get_rap_artists_old():
 	url = "http://rateyourmusic.com/list/ChrisPC/the_500_greatest_hip_hop_albums__plus_the_other_ones_that_are_honorable_mention_/"
 	results = []
 
@@ -52,8 +52,44 @@ def get_rap_artists():
 			results.append(str(a.text.encode('utf-8'))) #convert to utf for comprehension
 	return results
 
+
 """
-	Function which accepts an artists name and scrapes top 20 songs
+	Function which scrapes the top rap artists from 1989-2013 from Wikipedia
+	NOTE: The output is not perfect
+"""
+def get_rap_artists():
+	urls = [
+	"http://en.wikipedia.org/wiki/List_of_Billboard_number-one_rap_singles_of_the_1980s_and_1990s#1989",
+	"http://en.wikipedia.org/wiki/List_of_Billboard_number-one_rap_singles_of_the_2000s",
+	"http://en.wikipedia.org/wiki/List_of_Billboard_Hot_Rap_Songs_number-one_hits_of_the_2010s"
+	]
+
+	artists = []
+	
+	# go through each page
+	for url in urls:
+		source = hit_page(url)
+		# get the table with all the artists
+		rows = source.findAll('table')[2].findAll('tr')
+		row_count = 1
+
+		# hack to find the artists in a wikipedia table
+		while row_count < len(rows):
+			data = rows[row_count].findAll('td')
+			if row_count == 1:
+				artists.append(data[1].text.split(" featuring")[0])
+			else:
+				artists.append(data[0].text.split(" featuring")[0])
+			row_count += 1
+	
+	#write to disk
+	text_file = open("artists.txt", "w")
+	for artist in artists:
+		text_file.write(artist.encode('utf-8') + "\n")
+	text_file.close()
+
+"""
+	Function which accepts an artists name and return URLs for top 20 songs
 """
 def get_rap_songs(artist):
 	url = "http://rap.genius.com/"
@@ -77,19 +113,25 @@ def get_rap_songs(artist):
 	Function which accepts a link to a song's page on rap genius and scrapes
 	its lyrics in raw text
 """
-def get_rap_lyrics(song_link):
-	# we need to use selenium so the page actually populates
+def get_rap_lyrics(song_links):
+	out = ""
 	driver = webdriver.Firefox()
-	driver.get(song_link)
-	driver.implicitly_wait(5)
-	sleep(2*random()) # fake a human
-
-	lyrics = driver.find_elements_by_class_name('lyrics')
-	text = ""
-	for lyric in lyrics:
-		source = BeautifulSoup(lyric.get_attribute('innerHTML'))
-		text += source.text
-	text = raw_text(text)
-	driver.close()
-	return text
 	
+	#get the lyrics for each song
+	for song_link in song_links:
+		# we need to use selenium so the page actually populates
+		driver.get(song_link)
+		driver.implicitly_wait(5)
+		sleep(2*random()) # fake a human
+
+		lyrics = driver.find_elements_by_class_name('lyrics')
+		text = ""
+		for lyric in lyrics:
+			source = BeautifulSoup(lyric.get_attribute('innerHTML'))
+			text += source.text
+		text = raw_text(text)
+		out += text
+
+	driver.close()
+	return out
+		
